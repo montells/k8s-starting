a76c2700f69d1dcd60e7cda8cb5a322af0661123
Michel Sánchez Montells <michel.sanchez_montells@wassermeloni.de>
2026-03-21 11:02:42 +0100

    setup allow hosts for backend sinatra app

--- backend/sinatra.rb
+++ backend/sinatra.rb
@@ -6,6 +6,14 @@ require_relative 'version'
 set :port, 8081
 set :bind, '0.0.0.0'
 
+allowed_hosts = ENV.fetch("ALLOWED_HOSTS", "sinatra-backend-svc")
+
+unless allowed_hosts.empty?
+  set :host_authorization, {
+    permitted_hosts: allowed_hosts.split(",")
+  }
+end
+
 # Set JSON content type globally for all responses
 before do
   content_type :json
--- backend/version.rb
+++ backend/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module VERSION
-  STRING = '0.0.1'
+  STRING = '0.1.0'
 end
